a70332402ef05e2a9d166aebc53eebf7023f1099
Rodolfo A <Rodolfo A>
2017-03-13 20:10:38 -0600

    Modified sigmoid function 5/100 score

--- Week 3 - Logistic Regression/machine-learning-ex2/ex2/sigmoid.m
+++ Week 3 - Logistic Regression/machine-learning-ex2/ex2/sigmoid.m
@@ -1,3 +1,4 @@
+
 function g = sigmoid(z)
 %SIGMOID Compute sigmoid function
 %   g = SIGMOID(z) computes the sigmoid of z.
@@ -9,6 +10,7 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
+g = 1.0 ./ (1.0 + exp(-z))
 
 
 
