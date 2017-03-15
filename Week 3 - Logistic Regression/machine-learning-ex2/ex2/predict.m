ab00c56d54401d2d3ebd813fdf5a03380cee9f5e
Rodolfo A <Rodolfo A>
2017-03-14 18:35:08 -0600

    Added predict.m

--- Week 3 - Logistic Regression/machine-learning-ex2/ex2/predict.m
+++ Week 3 - Logistic Regression/machine-learning-ex2/ex2/predict.m
@@ -8,20 +8,16 @@ m = size(X, 1); % Number of training examples
 
 % You need to return the following variables correctly
 p = zeros(m, 1);
-
+/857
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
 
-
-
-
-
-
-
+p = sigmoid(X * theta) >= 0.5;
 % =========================================================================
 
 
 end
+    
