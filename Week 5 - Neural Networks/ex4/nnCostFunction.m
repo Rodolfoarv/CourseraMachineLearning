2ae67ded2833c201358f554fcccb7ce049dc2f8c
Rodolfo A <Rodolfo A>
2017-07-16 03:25:04 -0500

    ADDED: Regularization to neural network FINISHED ASSIGMENT 100/100

--- Week 5 - Neural Networks/ex4/nnCostFunction.m
+++ Week 5 - Neural Networks/ex4/nnCostFunction.m
@@ -121,7 +121,8 @@ Theta2_grad = (1/m) * Theta2_grad
     
 %% Part 3
 
-
+Theta1_grad = Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
+Theta2_grad = Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
 
 
 
