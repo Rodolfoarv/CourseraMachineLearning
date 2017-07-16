57e982c796d08af6d82f0c2466d6c602d2e2b1fd
Rodolfo A <Rodolfo A>
2017-07-16 03:20:25 -0500

    ADDED: Backpropagation implementation algorithm, progress 90/100

--- Week 5 - Neural Networks/ex4/nnCostFunction.m
+++ Week 5 - Neural Networks/ex4/nnCostFunction.m
@@ -90,7 +90,35 @@ J = J + regularizedTerm
 
 %% Part 2
 
+for t = 1:m
 
+	% For the input layer, where l=1:
+	a1 = [1; X(t,:)'];
+
+	% For the hidden layers, where l=2:
+	z2 = Theta1 * a1;
+	a2 = [1; sigmoid(z2)];
+
+	z3 = Theta2 * a2;
+	a3 = sigmoid(z3);
+
+	yy = ([1:num_labels]==y(t))';
+	% For the delta values:
+	delta_3 = a3 - yy;
+
+	delta_2 = (Theta2' * delta_3) .* [1; sigmoidGradient(z2)];
+	delta_2 = delta_2(2:end); % Taking of the bias row
+
+	% delta_1 is not calculated because we do not associate error with the input    
+
+	% Big delta update
+	Theta1_grad = Theta1_grad + delta_2 * a1';
+	Theta2_grad = Theta2_grad + delta_3 * a2';
+end
+
+Theta1_grad = (1/m) * Theta1_grad
+Theta2_grad = (1/m) * Theta2_grad
+    
 %% Part 3
 
 
