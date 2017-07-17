3228db9405266d3a722c314260f42dd322fd721d
Rodolfo A <Rodolfo A>
2017-07-17 17:26:21 -0500

    Added svm parameters with the cross validation set ( C and sigma ) for dataset #3, 50/100

--- Week 7 - Support Vector Machines/machine-learning-ex6/ex6/dataset3Params.m
+++ Week 7 - Support Vector Machines/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,8 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+results = eye(64,3);
+errorRow = 0;
 
+for C_values = [0.01, 0.03, 0.1, 0.3, 1, 3,10,30]
+    for sigma_values = [0.01, 0.03, 0.1, 0.3, 1, 3,10,30]
+        errorRow = errorRow + 1;
+        model = svmTrain(X, y, C_values, @(x1,x2) gaussianKernel(x1,x2,sigma_values));
+        predictions = svmPredict(model, Xval)
+        prediction_error = mean(double(predictions ~= yval));
+        results(errorRow, :) = [C_values, sigma_values, prediction_error];
+    end
+end
 
+sorted_results = sortrows(results, 3);
+C = sorted_results(1,1);
+sigma = sorted_results(1,2);
 
 
 
